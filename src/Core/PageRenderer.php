87de77def523b9a9dd836687ea0779ba7ca38dbf
XyloIsCoding <xylo.is.coding@gmail.com>
2026-08-26 16:47:22 +0200

    pages now always get a non-null PageData

--- src/Core/PageRenderer.php
+++ src/Core/PageRenderer.php
@@ -20,10 +20,10 @@ final class PageRenderer
         
         // Get the page data for the specified instance, or the first instance if none is specified.
         $pageData = $instanceSlug !== null ? Storage::findBySlug($typeSlug, $instanceSlug) : Storage::findFirst($typeSlug);
-        self::render($page->path(), $pageData);
+        self::render($page->path(), $pageData?? PageData::fromPage($page, '', ''));
     }
 
-    private static function render(string $filePath, ?PageData $data): void
+    private static function render(string $filePath, PageData $data): void
     {
         if (!file_exists($filePath)) {
             Router::notFound();
