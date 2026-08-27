b338221eccdfd410d4f6693de93173a95b10a51f
XyloIsCoding <xylo.is.coding@gmail.com>
2026-08-28 00:01:06 +0200

    fixed composite fileds style so that lable is outside the box

--- src/Fields/CompositeField.php
+++ src/Fields/CompositeField.php
@@ -20,13 +20,16 @@ abstract class CompositeField extends Field
     {
         ?>
         <div class="vcms-field vcms-field--composite">
-            <div class="vcms-field__label"><?= htmlspecialchars($this->config['label'] ?? 'value') ?></div>
-            <div class="vcms-field__group">
-            <?php
-            foreach ($this->getFields() as $fieldName => $field) {
-                $field->render("{$name}[{$fieldName}]");
-            }
-            ?></div>
+            <div class="vcms-field__label">
+                <?= htmlspecialchars($this->config['label'] ?? 'value') ?>
+                <div class="vcms-field__group">
+                <?php
+                foreach ($this->getFields() as $fieldName => $field) {
+                    $field->render("{$name}[{$fieldName}]");
+                }
+                ?>
+                </div>
+            </div>
         </div>
         <?php
     }
