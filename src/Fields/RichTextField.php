db674b7f3ffd39a10561efb938a5514bb44b8b2d
XyloIsCoding <xylo.is.coding@gmail.com>
2026-09-03 15:31:28 +0200

    added rich text field

---
+++ src/Fields/RichTextField.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace VanillaCms\Fields;
+
+use VanillaCms\Fields\Field;
+
+class RichTextField extends Field
+{
+    private string $value = '';
+
+    public function toArray(): array
+    {
+        return ['value' => $this->value];
+    }
+
+    public function fromArray(array $data): void
+    {
+        $this->value = $data['value'] ?? '';
+    }
+
+    public function render(string $name): void
+    {
+        ?>
+        <div class="vcms-field vcms-field--rich-text">
+            <label class="vcms-field__label">
+                <?= htmlspecialchars($this->config['label'] ?? 'value') ?>
+                <textarea class="vcms-field__input" name="<?= "{$name}[value]" ?>"><?= htmlspecialchars($this->value) ?></textarea>
+            </label>
+        </div>
+        <?php
+    }
+
+    public function getText(): string
+    {
+        return $this->value;
+    }
+}
