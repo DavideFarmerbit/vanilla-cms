69573f34684ed645b17414d244b36724ed56e382
XyloIsCoding <xylo.is.coding@gmail.com>
2026-09-04 17:06:48 +0200

    added support for --short modifier to RichTextField

--- src/Fields/RichTextField.php
+++ src/Fields/RichTextField.php
@@ -20,8 +20,9 @@ class RichTextField extends Field
 
     public function render(string $name): void
     {
+        $shortClass = !empty($this->config['short']) ? ' vcms-field--rich-text--short' : '';
         ?>
-        <div class="vcms-field vcms-field--rich-text">
+        <div class="vcms-field vcms-field--rich-text<?= $shortClass ?>">
             <label class="vcms-field__label">
                 <?= htmlspecialchars($this->config['label'] ?? 'value') ?>
                 <textarea class="vcms-field__input" name="<?= "{$name}[value]" ?>"><?= htmlspecialchars($this->value) ?></textarea>
